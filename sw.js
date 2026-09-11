20b8c83fddd3e731dbf59787c84b96ed4fbb5897
npjitteang-ux <np.jitteang@gmail.com>
2026-09-11 19:24:05 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("notificationclick",e=>{e.notification.close();e.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(cs=>cs.length?cs[0].focus():clients.openWindow("./")))})
